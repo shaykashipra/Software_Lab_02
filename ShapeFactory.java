04ed092d42a4764be2f9f9aea15c02188eddcd30
shaykashipra2 <triviatruism@gmail.com>
2024-01-25 15:37:13 +0600

    created

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -10,4 +10,15 @@ public class ShapeFactory {
     }
 
 
-
+        public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
